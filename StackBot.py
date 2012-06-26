d76d50a78d9d51455fc012e7be078ecc24c845ff
Quinlan P <qpfiffer@gmail.com>
2012-06-25 23:12:29 -0700

    Initial commit of redis stuff.

--- StackBot.py
+++ StackBot.py
@@ -4,6 +4,7 @@ from irc.client import nm_to_n
 import irc.bot
 import json
 import string
+import redis
 
 config_file_name = "config.txt"
 
@@ -14,7 +15,11 @@ class StackBot(irc.bot.SingleServerIRCBot):
         self.config_json = json.load(self.config_file_handle)
         self.config_file_handle.close()
 
-        self.server = [(self.config_json["server"], 6667)];
+        # Redis stuff
+        self.redis_handler = redis.Redis('localhost')
+
+        # Config stuff handled at runtime
+        self.server = [(self.config_json["server"], 6667)]
         self.nickname = self.config_json["nickname"]        
         self.realname = self.config_json["realname"]
         self.channel = self.config_json["channel"]
@@ -23,18 +28,19 @@ class StackBot(irc.bot.SingleServerIRCBot):
         # Initialize:
         irc.bot.SingleServerIRCBot.__init__(self, self.server, self.nickname, self.realname)
         
-        self.user_to_stack = {'nobody': ['nothing.', 'Get some work done.']}
         self.commands = ['Commands can be prefaced with stack or ' + self.nickname, 'stats: Prints some stack stats (users, stack depth, etc.)', 'mystack: Prints your own stack', 'push <text>: Pushes <text> onto your stack.', 'help: Really? Come on.', 'pop: Pops the top item of the stack'] 
        
     def on_welcome(self, c, e):
         c.join(self.channel, key=self.password)
 
     def on_pubmsg(self, c, e):
-        if self.nickname in e.arguments()[0] or 'stack' in e.arguments()[0]:
+        arguments = e.arguments()[0]
+
+        if self.nickname in arguments or 'stack' in arguments:
             # Make sure they were talking to us, and then print
             # their stack to the channel.
             user = nm_to_n(e.source())
-            self.do_command(e.arguments()[0], c, user, self.channel)
+            self.do_command(arguments, c, user, self.channel)
 
     def on_privmsg(self, c, e):
         # If this was a private message, just send the stack
@@ -44,47 +50,38 @@ class StackBot(irc.bot.SingleServerIRCBot):
 
     def print_stack(self, connection, user, channel):
         i = 0
-        # We need to reverse the stack, because everything happens at the end
-        # instead of the front.
-        self.user_to_stack[user].reverse()
-        for item in self.user_to_stack[user]:
+        user_items = self.redis_handler.lrange(user, 0, -1)
+        for item in user_items:
             connection.privmsg(self.channel, "%(item_index)i:%(item_text)s" % {"item_index": i, "item_text": item})
             i = i+1
-        # Make sure we put it back the way we found it:
-        self.user_to_stack[user].reverse()
         
     def do_command(self, command, connection, user, channel):
         to_lowered = string.lower(command)
         if 'stats' in to_lowered:
            print "Not implemented." 
            return
+
         elif 'mystack' in to_lowered:
-            if user in self.user_to_stack.keys():
-                self.print_stack(connection, user, channel)
-            else:
-                connection.privmsg(channel, "You're not in the stack yet.")
+            self.print_stack(connection, user, channel)
+
         elif 'push' in to_lowered:
-            stack_item = command.partition('push')
+            stack_item = command.partition('push')[2]
 
-            if user in self.user_to_stack.keys():
-                self.user_to_stack[user].append(stack_item[2])
-            else:
-                self.user_to_stack[user] = [stack_item[2]]
+            users_stack = self.redis_handler.lrange(user, 0, -1)
+            self.redis_handler.rpush(user, stack_item)
 
-            connection.privmsg(channel, "Added%(item)s to %(user)s\'s stack." % {"item": stack_item[2], "user": user})
+            connection.privmsg(channel, "Added%(item)s to %(user)s\'s stack." % {"item": stack_item, "user": user})
             return
+
         elif 'help' in to_lowered:
             for item in self.commands:
                 connection.privmsg(channel, item)
             return
 
         elif 'pop' in to_lowered:
-            if user in self.user_to_stack.keys() and len(self.user_to_stack[user]) > 0:
-                    self.user_to_stack[user].pop()
-                    self.print_stack(connection, user, channel)
-                    return
-
-            connection.privmsg(channel, "Nothing to pop, %(user)s." % {"user": user})
+            self.redis_handler.rpop(user)
+            self.print_stack(connection, user, channel)
+            
             return
 
 def main():
